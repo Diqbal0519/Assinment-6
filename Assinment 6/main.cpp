07b34d2373c187c78fa71fba1d48bfa689ec6ff2
Duha Iqbal <DIqbal0519@gmail.com>
2024-04-21 16:54:39 -0400

    Initial Commit

---
+++ Assinment 6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assinment 6
+//
+//  Created by Duha Iqbal on 4/21/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
